87adaa0db6ea582b7a30f7a722b4c21f3c11c75e
Jon Merilä <jon.merila97@hotmail.com>
2024-11-26 14:15:09 +0100

    Added more tests to products test

--- ChristmasShop/src/Components/Products/Products.test.tsx
+++ ChristmasShop/src/Components/Products/Products.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { beforeEach, describe, expect, it} from 'vitest';
-import {render, screen} from '@testing-library/react'
+import {render, screen, within} from '@testing-library/react'
 
 import Products from './Products';
 import { IProduct } from '../../Models/IProduct';
@@ -53,6 +53,18 @@ describe("Products comp", () => {
         const testIdItem = screen.getAllByTestId("product-item");
         expect(testIdItem).toHaveLength(testData.length);
     })
-
-    
+    it("should have a price for each product", () => {
+        const allProducts = screen.getAllByTestId("product-item");
+        allProducts.forEach((item) => {
+            const price = within(item).getByRole("strong");
+            expect(price).toBeInTheDocument();
+        })
+    })
+    it("should have a heading for each card", () => {
+        const allProducts = screen.getAllByTestId("product-item");
+        allProducts.forEach((item) => {
+            const heading = within(item).getByRole('heading', { level: 3 });
+            expect(heading).toBeInTheDocument();
+        })
+    })
 })
